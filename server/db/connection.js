56e50dee7ab4ced7cc9f4e0f41b979aa250d54e9
krrsh <gmkrishnaa63@gmail.com>
2025-03-15 19:34:01 +0530

    db url change

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://krshna:pass123@cluster0.ath0r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
+mongoose.connect('mongodb+srv://krshna:pass123@cluster0.ath0r.mongodb.net/wrktbuddy?retryWrites=true&w=majority&appName=Cluster0').then(()=>{
     console.log("Connection established")
 }).catch((err)=>{
     console.log("Not connected to DB, Error : ", err)
